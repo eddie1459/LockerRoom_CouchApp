4cc07df7df53f29b71850f5456a8802059b304c3
eddie1459 <eddie_d_2000@hotmail.com>
2012-08-10 22:07:53 -0500

    Added getTime function to the message view to show the time the comment was added

--- _attachments/js/views/message/message.js
+++ _attachments/js/views/message/message.js
@@ -19,8 +19,34 @@ define([
             var yyyy = currentDt.getFullYear();
             var date = mm + '/' + dd + '/' + yyyy;
             content.date = date;
+            content.time = this.getTime(currentDt);
             $(this.el).html(this.template(content));
             return this;
+        },
+        getTime: function(date) {
+            var a_p = "";
+            var curr_hour = date.getHours();
+
+            if (curr_hour < 12)
+            {
+                a_p = "AM";
+            }
+            else
+            {
+                a_p = "PM";
+            }
+            if (curr_hour == 0)
+            {
+                curr_hour = 12;
+            }
+            if (curr_hour > 12)
+            {
+                curr_hour = curr_hour - 12;
+            }
+
+            var curr_min = date.getMinutes();
+            var timeToret = curr_hour + ":" + curr_min + " " + a_p;
+            return timeToret;
         }
     });
     return view;
